eb91b6c73555ffcb46a486005f3c570f83b48cdf
trungtin03 <trungtintran1103@gmail.com>
2024-12-05 14:56:39 +0700

    add

--- TicketSupport/Areas/Admin/Controllers/LoginController.cs
+++ TicketSupport/Areas/Admin/Controllers/LoginController.cs
@@ -1,82 +1,88 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Helpers;
+using System.Drawing;
 using System.Web.Mvc;
+using System.Web.Helpers;
+using System.Linq;
 using TicketSupport.Models;
+using System.Drawing.Imaging;
+using System.IO;
 
 namespace TicketSupport.Areas.Admin.Controllers
 {
+    
     public class LoginController : Controller
     {
-        // GET: Admin/Login
         public ActionResult Index()
         {
             return View();
         }
-        [HttpGet]
-        public ActionResult Register()
+        // Action để tạo CAPTCHA
+        public ActionResult GenerateCaptcha()
         {
-            return View();
-        }
+            string captchaCode = GenerateRandomCode(6); // Số ký tự của mã CAPTCHA
+            Session["Captcha"] = captchaCode; // Lưu mã CAPTCHA vào Session
 
-        [HttpPost]
-        public ActionResult Register(string username, string email, string password)
-        {
-            using (var db = new Tech_Support_TicketEntities())
+            using (var bitmap = new Bitmap(200, 50))
+            using (var graphics = Graphics.FromImage(bitmap))
             {
-                // Kiểm tra tài khoản đã tồn tại
-                if (db.tblnguoidungs.Any(u => u.email == email))
+                // Tạo màu nền và màu văn bản
+                graphics.Clear(Color.White);
+                using (var font = new Font("Arial", 20, FontStyle.Bold))
                 {
-                    ViewBag.Message = "Username already exists.";
-                    return View();
+                    using (var brush = new SolidBrush(Color.Black))
+                    {
+                        graphics.DrawString(captchaCode, font, brush, new PointF(10, 10));
+                    }
                 }
 
-                // Tạo tài khoản mới
-                var user = new tblnguoidung
+                // Thêm các đường nét ngẫu nhiên để làm khó nhận diện
+                var pen = new Pen(Color.Gray, 2);
+                for (int i = 0; i < 5; i++)
                 {
-                    ten_nguoi_dung = username,
-                    email = email,
-                    password = BCrypt.Net.BCrypt.HashPassword(password) // Hash mật khẩu
-                };
+                    graphics.DrawLine(pen, new PointF(i * 40, 0), new PointF(i * 40, 50));
+                }
 
-                db.tblnguoidungs.Add(user);
-                db.SaveChanges();
-                ViewBag.Message = "Đăng kí thành công";
-                return RedirectToAction("Index");
+                using (var memoryStream = new MemoryStream())
+                {
+                    bitmap.Save(memoryStream, ImageFormat.Png);
+                    return File(memoryStream.ToArray(), "image/png");
+                }
             }
         }
 
-        [HttpGet]
-        public ActionResult Login()
+        private string GenerateRandomCode(int length)
         {
-            return View();
+            var random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
         }
 
         [HttpPost]
-        public ActionResult Login(string email, string password)
+        public ActionResult Login(string email, string password, string captcha)
         {
-            var db = new Tech_Support_TicketEntities();
-            
+            // Kiểm tra mã CAPTCHA
+            if (Session["Captcha"] == null || Session["Captcha"].ToString() != captcha)
+            {
+                ViewBag.Message = "CAPTCHA không hợp lệ. Vui lòng thử lại.";
+                return View();
+            }
+
+            using (var db = new Tech_Support_TicketEntities())
+            {
                 var user = db.tblnguoidungs.FirstOrDefault(u => u.email == email);
 
-                //if (user == null || !BCrypt.Net.BCrypt.Verify(password, user.password))
-                //{
-                //    ViewBag.Message = "Invalid username or password.";
-                //    return View();
-                //}
-                //if (user == null )
-                //{
-                //    ViewBag.Message = "Invalid username or password.";
-                //    return View("Index");
-                //}
+                if (user == null || !BCrypt.Net.BCrypt.Verify(password, user.password))
+                {
+                    ViewBag.Message = "Invalid username or password.";
+                    return View();
+                }
+
                 // Lưu thông tin đăng nhập vào Session
                 Session["UserId"] = user.ma_nguoi_dung;
                 Session["Username"] = user.email;
-                return RedirectToAction("Index", "Home");
-            
-        }
 
+                return RedirectToAction("Index", "Home");
+            }
+        }
     }
 }
---
+++ TicketSupport/ViewModels/CaptchaResponse.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+public class CaptchaResponse
+{
+    [JsonProperty("success")]
+    public bool Success { get; set; }
+
+    [JsonProperty("challenge_ts")]
+    public DateTime ChallengeTimestamp { get; set; }
+
+    [JsonProperty("hostname")]
+    public string Hostname { get; set; }
+}
